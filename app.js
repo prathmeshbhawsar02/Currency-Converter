0b6b32910f01044e53a6f63e3d97ffa9c6c6a1b7
prathmeshbhawsar02 <prathmeshbhawsar02@gmail.com>
2025-09-13 14:31:20 +0530

    domloaded realtime default currency update

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const BASE_URL =
-  "https://v6.exchangerate-api.com/v6/155563dd3d59538de01f182a/latest/";// API's base url with api key
+  "https://v6.exchangerate-api.com/v6/155563dd3d59538de01f182a/latest/"; // API's base url with api key
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 console.log(dropdowns);
 const btn = document.querySelector("form button");
-const fromCurr=document.querySelector(".from select");
-const toCurr=document.querySelector(".to select");
-const msg=document.querySelector(".msg");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  let amount = document.querySelector(".amount input");
+  let amtValue = amount.value;
+
+  if (amtValue === "" || amtValue <= 0) {
+    amtValue = 1;
+    amount.value = 1;
+  }
+
+  const url = `${BASE_URL}${fromCurr.value}`; //custom api call according to fromCurr value
+
+  let response = await fetch(url); // fetching api
+  let data = await response.json(); // getting api response
+
+  let from = fromCurr.value;
+  let to = toCurr.value;
+  let rate = data["conversion_rates"][to];
+  console.log(rate);
+
+  let finalAmt = amtValue * rate;
+  msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
+});
 
 for (let select of dropdowns) {
   for (currCode in countryList) {
@@ -38,24 +61,22 @@ btn.addEventListener("click", async (event) => {
   event.preventDefault();
   let amount = document.querySelector(".amount input");
   let amtValue = amount.value;
-  
-  if ((amtValue === "" || amtValue <= 0)) {
+
+  if (amtValue === "" || amtValue <= 0) {
     amtValue = 1;
     amount.value = 1;
   }
-  
-  
-  const url=`${BASE_URL}${fromCurr.value}`; //custom api call according to fromCurr value
 
-  let response=await fetch(url);// fetching api
-  let data=await response.json();// getting api response
+  const url = `${BASE_URL}${fromCurr.value}`; //custom api call according to fromCurr value
 
-  let from=fromCurr.value;
-  let to=toCurr.value;
-   let rate = data["conversion_rates"][to];
-   console.log(rate);
-   
-   let finalAmt= amtValue*rate;
-   msg.innerText=`${amtValue} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
-  
+  let response = await fetch(url); // fetching api
+  let data = await response.json(); // getting api response
+
+  let from = fromCurr.value;
+  let to = toCurr.value;
+  let rate = data["conversion_rates"][to];
+  console.log(rate);
+
+  let finalAmt = amtValue * rate;
+  msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
 });
